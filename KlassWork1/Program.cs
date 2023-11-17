37fdd227fc4d808712dcc3183b2da2f323fdf47f
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-17 21:22:01 +0300

    Task 2

--- KlassWork1/Program.cs
+++ KlassWork1/Program.cs
@@ -1,4 +1,6 @@
-﻿System.Console.WriteLine("Input Numbers1");
+﻿/*
+Задача 1
+System.Console.WriteLine("Input Numbers1");
 int num1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Input Numbers2");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -9,3 +11,12 @@ if (num1 == Math.Pow(num2,2)) {
 else {
     Console.WriteLine("False");
 }
+Задача 2
+System.Console.WriteLine("Input Numbers");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+while (i <= num) {
+    Console.WriteLine(i);
+    i ++;
+}
+*/
