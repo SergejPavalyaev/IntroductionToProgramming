ceda45816f235cf4aa4b83783b299d0e23ca9e32
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-19 21:59:24 +0300

    Task1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Первое число `5` меньше чем второе число `6`
+int firstNumber = 10;
+int secondNumber = 6;
+if (firstNumber < secondNumber) {
+    System.Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+}
+if (firstNumber > secondNumber) {
+    System.Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+}
+if (firstNumber == secondNumber) {
+    System.Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+}
