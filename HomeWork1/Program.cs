3dda000d56298d34c809f5f019feb84298d838e9
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-19 22:19:19 +0300

    Task2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,12 +1,24 @@
 ﻿// Первое число `5` меньше чем второе число `6`
-int firstNumber = 10;
-int secondNumber = 6;
-if (firstNumber < secondNumber) {
-    System.Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
-}
-if (firstNumber > secondNumber) {
-    System.Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
-}
-if (firstNumber == secondNumber) {
-    System.Console.WriteLine($"Введенные числа равны `{firstNumber}`");
-}
+// int firstNumber = 10;
+// int secondNumber = 6;
+// if (firstNumber < secondNumber) {
+//     System.Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+// }
+// if (firstNumber > secondNumber) {
+//     System.Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+// }
+// if (firstNumber == secondNumber) {
+//     System.Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+// }
+// int a = 5;
+// int b = 6;
+// int c = 7;
+// int max = a;
+// if (max < b){
+//     max = b;
+// }
+// if  (max < c){
+//     max = c;
+// }
+// System.Console.WriteLine($"Максимальное введенное число `{max}`");
+//для возвращения значения из метода используют return
