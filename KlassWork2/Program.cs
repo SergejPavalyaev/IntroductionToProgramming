680df8dce9c97624717254e2bf5728f29360844c
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-20 21:34:10 +0300

    Task 2

--- KlassWork2/Program.cs
+++ KlassWork2/Program.cs
@@ -7,18 +7,21 @@ namespace KlassWork2
             string ? numString = Console.ReadLine();
             // int numInt = Int32.Parse(numString); - перевод строки в число
             int numInt;
-            while(! Int32.TryParse(numString, out numInt)){// перевод строки в число с проверкой ввода с консоли
+            while (! Int32.TryParse(numString, out numInt)){// перевод строки в число с проверкой ввода с консоли
                 System.Console.WriteLine("Error, try again");
                 numString = Console.ReadLine();
             }
-            int result = DeletDigit(numInt);
+            if (numInt > 99 && numInt < 1000) {
+            int result = MathPow(numInt);
             System.Console.WriteLine(result);
+            }
+            
         }
-        public static int DeletDigit(int num){
-            int result;
-            int firstDigit = num / 100;
-            int secondDigit = num % 10;
-            result = firstDigit * 10 + secondDigit;
+        public static int MathPow(int num){
+            int temp = num % 10;
+            int secondDigit = temp % 10;
+            int therdDigit = num % 10;
+            int result = (int) Math.Pow (secondDigit, therdDigit);
             return result;
         }
 }
