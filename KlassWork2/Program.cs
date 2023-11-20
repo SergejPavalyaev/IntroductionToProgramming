0948216f9f4c281b9d4f219235c73789b0b8c7fb
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-20 21:10:06 +0300

    Task 1

--- KlassWork2/Program.cs
+++ KlassWork2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 5
+namespace KlassWork2
+{
+    public class Program{
+        public static void Main (string [] args){
+            System.Console.Write("Input number => ");
+            string ? numString = Console.ReadLine();
+            // int numInt = Int32.Parse(numString); - перевод строки в число
+            int numInt;
+            while(! Int32.TryParse(numString, out numInt)){// перевод строки в число с проверкой ввода с консоли
+                System.Console.WriteLine("Error, try again");
+                numString = Console.ReadLine();
+            }
+            int result = DeletDigit(numInt);
+            System.Console.WriteLine(result);
+        }
+        public static int DeletDigit(int num){
+            int result;
+            int firstDigit = num / 100;
+            int secondDigit = num % 10;
+            result = firstDigit * 10 + secondDigit;
+            return result;
+        }
+}
+}
