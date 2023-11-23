df85ab9f046d650c7cbd9d5d7a631db4ce894ae6
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-23 22:17:13 +0300

    KlassWork_Task

---
+++ KlassWork3_Task/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace KlassWork3
+{
+    public class Program
+    {
+        public static void Main(string [] arg){
+            //Task2
+            Random rand = new Random();
+            int size = rand.Next(5, 10);
+            int [] array = new int[size];
+            array = InitArray (size);
+            PrintArray(array);
+            
+        }
+        public static int [] NumInArray (int num){
+
+        }  
+        public static int [] InitArray (int size) {
+            int [] array = new int [size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(-10,11);
+            }
+            return array;
+        }
+        public static void PrintArray (int [] array){
+            foreach (int item in array){
+                System.Console.Write(item + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
