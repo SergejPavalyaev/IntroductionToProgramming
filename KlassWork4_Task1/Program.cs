9059fc14447fa8cb42c408862650c8b3056223f2
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-27 21:14:52 +0300

    Task_1

--- KlassWork4_Task1/Program.cs
+++ KlassWork4_Task1/Program.cs
@@ -3,7 +3,37 @@
     public class Program
     {
         public static void Main (string [] args){
-            System.Console.WriteLine("test");
+            System.Console.WriteLine("Input size array => ");
+            int size = InputNumber();
+            System.Console.WriteLine("Input array => ");
+            int [] array = new int [size];
+            for (int i = 0; i < size; i++)
+            {
+                array [i] = InputNumber();
+            }
+            int divide = 7;
+            int end = 1;
+            int count = DivideNumberEndNumber(array, divide, end);
+            System.Console.WriteLine($"Result = {count}");
+
+        }
+        
+        public static int InputNumber (){
+            string ? numString = Console.ReadLine();
+            int numInt;
+            while (! Int32.TryParse(numString, out numInt)){// перевод строки в число с проверкой ввода с консоли
+                System.Console.WriteLine("Error, try again");
+                numString = Console.ReadLine();
+            }
+            return numInt;
+        }
+        public static int DivideNumberEndNumber (int [] array, int divide, int end){
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item % 10 == end & item % 7 == 0) count++;
+            }
+            return count;
         }
     }
 }
