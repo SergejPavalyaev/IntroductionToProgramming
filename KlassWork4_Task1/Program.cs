bdb5e5feb8424bf84f7b87b11bc881b9832b9fbe
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-27 20:28:37 +0300

    Start

---
+++ KlassWork4_Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace KlassWork4_Task1
+{
+    public class Program
+    {
+        public static void Main (string [] args){
+            System.Console.WriteLine("test");
+        }
+    }
+}
